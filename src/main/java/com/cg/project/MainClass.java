bcb9e171c55e40582d3bea14450a80eb4d33359d
Shashwat Vats <shashwat.singh.vats@gmail.com>
2020-09-01 11:14:31 +0530

    First Maven Project Created

---
+++ src/main/java/com/cg/project/MainClass.java
@@ -0,0 +1,9 @@
+package com.cg.project;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		System.out.println("this is the main class");
+	}
+
+}
---
+++ src/test/java/com/cg/project/MainTest.java
@@ -0,0 +1,21 @@
+package com.cg.project;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class MainTest {
+
+	@Test
+	public void test1() {
+		Assert.fail("test1 has failed");
+	}
+	@Test
+	public void test2() {
+	}
+	@Test
+	public void test3() {
+		Assert.fail("test1 has failed");
+	}
+
+}
